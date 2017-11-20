b9711a27b24ef469c05aa30b5f97ad40ddc29530
Youngmin Kim <rladudals02@gmail.com>
2017-11-20 19:00:40 +0900

    0.1.6 Model
    1) Add message logging
    - watson result
    - response
    2) Add async module

--- controllers/plusfriend.ctrl.js
+++ controllers/plusfriend.ctrl.js
@@ -1,9 +1,11 @@
 
+var async = require('async');
 
 var messageController = require('./watson/conversation.ctrl');
 
 var userModel = require('../models/user.model');
 var chatRoomModel = require('../models/chat_room.model');
+var messageModel = require('../models/message.model');
 
 exports.loadKeyborad = function(callback){
 	var resultObject = new Object({});
@@ -24,46 +26,67 @@ exports.loadKeyborad = function(callback){
 };
 
 exports.dialogize = function(userKey, type, content, callback){
-  var resultObject = new Object({});
-
 	var textObject = new Object({});
 	textObject.text = content;
 
-	messageController.getConversationResponse(textObject, null, function(error, data){
-		var messageObject = new Object({});
-		var keyboardObject = new Object({});
+	var watsonData = "";
 
-		var text = "";
-		if(error){
-			text = "잘 모르겠어요..";
-		}else{
-			text = data.output.text[0];
+	async.parallel({
+		requestDB: function(callback){
+			messageModel.addRequestMessage(userKey, type, content, function(error, data){
+				callback(null, data);
+			});
+		},
+		response: function(callback){
+			messageController.getConversationResponse(textObject, null, function(error, data){
+				watsonData = data.output.text[0];
+
+				makeResponse(content, watsonData, function(error, resultObject){
+					callback(null, resultObject);
+				});
+			});
 		}
+	}, function(error, results){
+		var requestId = results.requestDB.insertId;
+		var response = results.response.message.text;
 
-		if(content == "사만다, 넌 누구니?"){
- 			text = "안녕하세요. 저는 대화의 문맥을 기억하는 봇입니다.";
- 		}else if(content == "사만다, 넌 어떻게 도와줄 수 있지?"){
- 			text = "당신의 관심사에 대한 정보를 알려줄 수 있습니다.";
- 		}else if(content == "사만다, 너에 대해 더 알고 싶어"){
- 			text = "영광이에요!";
-
-			var buttonObject = new Object({});
-			buttonObject.label = "공식 홈페이지 링크";
-			buttonObject.url = "http://13.124.56.85:12000";
-
-			messageObject.message_button = buttonObject;
-		}
-
-		messageObject.text = text;
-
-		keyboardObject.type = "text";
-
-		resultObject.message = messageObject;
-		resultObject.keyboard = keyboardObject;
-
-		callback(null, resultObject);
+		messageModel.addResponse(requestId, watsonData, response, function(error, resultObject){
+			callback(null, results.response);
+		});
 	});
 };
+
+function makeResponse(content, text, callback){
+	var resultObject = new Object({});
+
+	var messageObject = new Object({});
+	var keyboardObject = new Object({});
+
+	var response = text;
+
+	if(content == "사만다, 넌 누구니?"){
+		text = "안녕하세요. 저는 대화의 문맥을 기억하는 봇입니다.";
+	}else if(content == "사만다, 넌 어떻게 도와줄 수 있지?"){
+		text = "당신의 관심사에 대한 정보를 알려줄 수 있습니다.";
+	}else if(content == "사만다, 너에 대해 더 알고 싶어"){
+		text = "영광이에요!";
+
+		var buttonObject = new Object({});
+		buttonObject.label = "공식 홈페이지 링크";
+		buttonObject.url = "http://13.124.56.85:12000";
+
+		messageObject.message_button = buttonObject;
+	}
+
+	messageObject.text = text;
+
+	keyboardObject.type = "text";
+
+	resultObject.message = messageObject;
+	resultObject.keyboard = keyboardObject;
+
+	callback(null, resultObject);
+}
 
 exports.addFriend = function(userKey, callback){
   userModel.addUser(userKey, function(error, resultObject){
--- models/message.model.js
+++ models/message.model.js
@@ -1,19 +1,55 @@
 
+var async = require('async');
+
 var modelLog = "Message";
 var errorModel = require('./error.model');
 
 var queryModel = require('./query.model');
 
-exports.addRequestMessage = function(type, content, callback){
+exports.addRequestMessage = function(userKey, type, content, callback){
   console.log("addRequestMessage");
 
   var sql = "INSERT message (type_sn, content_ln) VALUE (?, ?)";
 
   var sqlParams = [type, content];
 
-  queryModel.request("insert", modelLog, sql, sqlParams, function(error, resultObject){
-    console.log(resultObject);
+  queryModel.request("insert", modelLog, sql, sqlParams, function(error, insertObject){
+    var messageId = insertObject.insertId;
 
-    callback(error, resultObject);
+    sql = "INSERT request (user_id, message_id, request_dtm) VALUE ((SELECT user_id FROM user WHERE kakao_key_sn = ?), ?, NOW())";
+
+    sqlParams = [userKey, messageId];
+
+    queryModel.request("insert", modelLog, sql, sqlParams, function(error, resultObject){
+      callback(error, resultObject);
+    });
+  });
+};
+
+exports.addResponse = function(requestId, watsonData, response, callback){
+  async.parallel({
+    watsonDataDB: function(callback){
+      var sql = "INSERT watson_result (request_id, content_ln) VALUE (?, ?)";
+
+      sqlParams = [requestId, watsonData];
+
+      queryModel.request("insert", modelLog + "WatsonData", sql, sqlParams, function(error, resultObject){
+        callback(error, resultObject);
+      });
+    },
+    responseDataDB: function(callback){
+      // TODO modify state
+      var state = "normal";
+
+      var sql = "INSERT response (request_id, state_sn, content_ln, response_dtm) VALUE (?, ?, ?, NOW())";
+
+      sqlParams = [requestId, state, response];
+
+      queryModel.request("insert", modelLog + "Response", sql, sqlParams, function(error, resultObject){
+        callback(error, resultObject);
+      });
+    }
+  }, function(error, results){
+    callback(null, true);
   });
 };
